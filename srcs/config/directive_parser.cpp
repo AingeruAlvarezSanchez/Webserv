5e707faf89fbd5b163728724051316271096856b
AingeruAlvarezSanchez <aingerualvarezsanchez@gmail.com>
2023-06-17 00:36:26 +0200

    syntax simplified for reuse, location rules parsing in progress

--- srcs/config/directive_parser.cpp
+++ srcs/config/directive_parser.cpp
@@ -1,11 +1,16 @@
 #include <cerrno>
 #include "../classes/server_info.hpp"
 
-static std::string erase_directive_delimiters(std::string const& serverDirective) {
-    size_t  directiveStart = serverDirective.find_first_of('{');
-    size_t  ruleNewLine = serverDirective.find_first_of('\n');
+static std::string erase_directive_delimiters(std::string const& directive, bool const& directiveType) {
+    size_t  directiveStart;
+    if (directiveType == 0) {
+        directiveStart = directive.find_first_of('{');
+    } else {
+        directiveStart = directive.find_first_of('[');
+    }
 
-    std::string line = serverDirective.substr(directiveStart + 1, (ruleNewLine - directiveStart - 1));
+    size_t  ruleNewLine = directive.find_first_of('\n');
+    std::string line = directive.substr(directiveStart + 1, (ruleNewLine - directiveStart - 1));
     size_t  lineComment = line.find_first_of('#');
 
     if (line.find_first_not_of(' ') != std::string::npos
@@ -13,35 +18,25 @@ static std::string erase_directive_delimiters(std::string const& serverDirective
         errno = 134;
         throw   ServerInfo::BadSyntax("Error: Webserv: Bad syntax"); //TODO maybe as a detail i can give the exact point of error with join
     }
-    std::string result = serverDirective.substr((ruleNewLine + 1), (serverDirective.length() - (ruleNewLine + 1) - 1));
+    std::string result = directive.substr((ruleNewLine + 1), (directive.length() - (ruleNewLine + 1) - 1));
     return result;
 }
 
 #include <iostream> //TODO
-#include <stdlib.h> //TODO
 //TODO if something is before the rule name (somelocation:, somelisten:) it must throw an exception, it doesnt atm
-static ServerInfo::locationDirective    get_location_conf(std::string & locationContent) {
-    size_t locationContentStart = locationContent.find_first_of('[');
+//TODO if something is after the location ] it must throw an exception, it doesnt atm
+static ServerInfo::locationDirective    get_location_conf(std::string & locationDirective) {
+    size_t locationContentStart = locationDirective.find_first_of('[');
+    size_t locationPathStart = locationDirective.find_first_not_of("location: ");
 
-
-    if (locationContentStart != locationContent.find_last_of('[')) {
-        errno = 134;
-        throw   ServerInfo::BadSyntax("Error: Webserv: Bad syntax"); //TODO maybe as a detail i can give the exact point of error with join
-    }
-
-    size_t locationPathStart = locationContent.find_first_not_of("location: ");
-    std::string locationPath = locationContent.substr(locationPathStart, (locationContentStart - locationPathStart));
+    std::string locationPath = locationDirective.substr(locationPathStart, (locationContentStart - locationPathStart));
     size_t locationPathEnd = locationPath.find_first_of(' ');
-    locationContent.erase(0, locationContentStart + 1);
-
     locationPath.erase(locationPathEnd, locationPath.length());
-    //TODO use template
 
-    std::cout << "directive content: |" << locationContent << "|\n";
+    locationDirective = erase_directive_delimiters(locationDirective, 1);
 
     ServerInfo::s_location location = {};
-    exit(1);
-    return  std::make_pair(locationPath, location); //TODO
+    return std::make_pair(locationPath, location); //TODO
 }
 
 static bool get_rule_conf(std::string const& line, size_t const& ruleSemicolon) {
@@ -57,7 +52,7 @@ static bool get_rule_conf(std::string const& line, size_t const& ruleSemicolon) 
 }
 
 ServerInfo::s_serverData   get_directive_conf(std::string & serverDirective) {
-    serverDirective = erase_directive_delimiters(serverDirective);
+    serverDirective = erase_directive_delimiters(serverDirective, 0);
 
     ServerInfo::s_serverData data = {}; //TODO
     if (serverDirective.find("listen:") == std::string::npos
@@ -65,7 +60,7 @@ ServerInfo::s_serverData   get_directive_conf(std::string & serverDirective) {
         errno = 134;
         throw   ServerInfo::BadSyntax("Error: Webserv: Bad syntax"); //TODO maybe as a detail i can give the exact point of error with join
     }
-    while (!serverDirective.empty()) {
+    while (!serverDirective.empty() && serverDirective.find_first_not_of(" \n") != std::string::npos) {
         std::string line = serverDirective.substr(0, serverDirective.find_first_of('\n') + 1);
         size_t ruleSemicolon = line.find_first_of(';');
 
@@ -78,6 +73,7 @@ ServerInfo::s_serverData   get_directive_conf(std::string & serverDirective) {
                 size_t locationEnd = serverDirective.find_first_of(']');
                 std::string locationDirective = serverDirective.substr(locationStart,((locationEnd + 1) - locationStart));
 
+                serverDirective.erase(locationStart, locationDirective.length() + 1);
                 data.serverLocations.push_back(get_location_conf(locationDirective));
             } else {
                 get_rule_conf(line, ruleSemicolon);
